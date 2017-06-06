d3b91bfb482d2e58f172d92b1f22131942419429
Jonathan Livingstone <seagull127@ymail.com>
2017-06-06 18:57:44 +0100

    Removing some old TODOs.

--- src/main.cpp
+++ src/main.cpp
@@ -10,41 +10,9 @@
   ===================================================================================================*/
 
 /* TODO(Jonny):
-    - struct to tuple.
-    - std::map, std::unordered_map, std::set, std::array.
-    - Struct meta data.
-        - Have a way to test if a member is private or not.
-        - An alternative to pp::print and pp::serialize that hide private members.
-        - It breaks if you use a comma to declare members of the same type.
-        - Have a way to get the type of different elements (as strings or types).
-        - Get a get_member(v, i) function, which simple returns the member at index i.
-        - Output a _useful_ error message if the user declares the same struct twice.
-        - In serialize struct, if there is a member which is an enum, call enum_to_string on it's value.
-        - Create bool is_primitive(T) which returns if something is a primitive or not.
-    - Union meta data.
-        - Simple version of struct.
-    - Function meta data.
-        - Get name (as string).
-        - Get linkage (as string).
-        - Get return type.
-        - Get param count.
-        - Get params types, and names as strings.
-    - References.
-    - Templates.
+    - In serialize struct, if there is a member which is an enum, call enum_to_string on it's value.
     - Allow mathematical macros (1 + 1) to be the index for an array.
     - Global consts for arrays.
-    - Handle typedefs.
-    - Base type macro. If the programmer enters non-pointer (or non reference) value, just return the same value.
-    - Make a is_primitive function.
-    - Make a function tell if something's a pointer or not. Could return false if not a pointer, and a positive integer
-      for the level of pointer otherwise. Should work with references too.
-    - If the user puts a directory in front of the file name ("dir/source.cpp") then the outputted code will get
-      placed in the directories parent, not the directory with the code.
-    - I could handle user-defined containers, assuming they conform to some strict set of rules. These could be:
-        - Does it implement the nessessary member functions to use the C++11 range-based for loops?
-        - It it a template struct where the template can only take one type?
-    - I don't think #if 1 #else blocks work correctly...
-    - A function which gets a pointer to a base class, from a normal class?
 */
 
 enum SwitchType {
