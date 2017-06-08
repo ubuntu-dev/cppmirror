78273fb7e3ed515d0eb5b6dde87b93cb551d50b2
Jonathan Livingstone <seagull127@ymail.com>
2017-06-08 20:54:10 +0100

    Added a header.

--- src/build.cpp
+++ src/build.cpp
@@ -1,3 +1,14 @@
+/*===================================================================================================
+  File:                    build.cpp
+  Author:                  Jonathan Livingstone
+  Email:                   seagull127@ymail.com
+  Licence:                 Public Domain
+                           No Warranty is offered or implied about the reliability,
+                           suitability, or usability
+                           The use of this code is at your own risk
+                           Anyone can use this code, modify it, sell it to terrorists, etc.
+  ===================================================================================================*/
+
 // Standard
 #include <stdint.h>
 #include <stdarg.h>  // va_list.
