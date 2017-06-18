c1b5b13ce4e5830e0f953818c28db5941464b3bd
Jonathan Livingstone <seagull127@ymail.com>
2017-06-18 09:00:45 +0100

    Relaced the order for generating C code vs. generating C++ code.

--- win32_msvc.bat
+++ win32_msvc.bat
@@ -27,6 +27,17 @@ popd
 
 rem Run after building.
 
+rem Test code for C.
+if "%RUN_TEST%"=="true" (
+    pushd "test"
+    "../build/mirror.exe" -c test_code.c
+    popd
+
+    pushd "build"
+    cl -Fetest %DEBUG_COMMON_COMPILER_FLAGS% -Wall "../test/test_code.c" -FmTest.map -link -subsystem:console,5.2 kernel32.lib
+    popd
+)
+
 rem Test code for C++.
 if "%RUN_TEST%"=="true" (
     pushd "test"
@@ -35,17 +46,5 @@ if "%RUN_TEST%"=="true" (
 
     pushd "build"
     cl -Fetest %DEBUG_COMMON_COMPILER_FLAGS% -Wall "../test/test_code.cpp" -FmTest.map -link -subsystem:console,5.2 kernel32.lib
-    popd
-)
-
-rem Test code for C.
-
-if "%RUN_TEST%"=="true" (
-    pushd "test"
-    "../build/mirror.exe" -c test_code.c
-    popd
-
-    pushd "build"
-    cl -Fetest %DEBUG_COMMON_COMPILER_FLAGS% -Wall "../test/test_code.c" -FmTest.map -link -subsystem:console,5.2 kernel32.lib
     popd
 )
